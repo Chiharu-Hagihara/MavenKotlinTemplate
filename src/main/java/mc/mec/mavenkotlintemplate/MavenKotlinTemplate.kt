3be416639d9bc37aa234b7d22b6ae8e4ed7b04de
M-E-C <Mr_El_Capitan@outlook.jp>
2020-05-04 21:40:12 +0900

    HoverTextを追加。

--- src/main/java/mc/mec/mavenkotlintemplate/MavenKotlinTemplate.kt
+++ src/main/java/mc/mec/mavenkotlintemplate/MavenKotlinTemplate.kt
@@ -1,6 +1,11 @@
 package mc.mec.mavenkotlintemplate
 
+import mc.mec.mavenkotlintemplate.commands.HovertextCommand
 import mc.mec.mavenkotlintemplate.commands.MavenCommand
+import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.ComponentBuilder
+import net.md_5.bungee.api.chat.HoverEvent
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 
 class MavenKotlinTemplate : JavaPlugin() {
@@ -8,8 +13,25 @@ class MavenKotlinTemplate : JavaPlugin() {
         logger.info("HelloWorld!")
 
         getCommand("maven")?.setExecutor(MavenCommand)
+        getCommand("hovertext")?.setExecutor(HovertextCommand)
     }
 
     override fun onDisable() { // Plugin shutdown logic
+    }
+
+    //  マインクラフトチャットに、ホバーテキストや、クリックコマンドを設定する関数
+    // [例1] sendHoverText(player,"ここをクリック",null,"/say test")
+    // [例2] sendHoverText(player,"カーソルをあわせて","ヘルプメッセージとか",null)
+    // [例3] sendHoverText(player,"カーソルをあわせてクリック","ヘルプメッセージとか","/say test")
+    fun sendHoverText(p: Player?, text: String, hoverText: String, command: String) {
+        //////////////////////////////////////////
+        //      ホバーテキストとイベントを作成する
+        val hoverEvent = HoverEvent(HoverEvent.Action.SHOW_TEXT, ComponentBuilder(hoverText).create())
+
+        //////////////////////////////////////////
+        //   クリックイベントを作成する
+        val clickEvent = ClickEvent(ClickEvent.Action.RUN_COMMAND, "/$command")
+        val message = ComponentBuilder(text).event(hoverEvent).event(clickEvent).create()
+        p?.spigot()?.sendMessage(*message)
     }
 }
--- src/main/java/mc/mec/mavenkotlintemplate/commands/HovertextCommand.kt
+++ src/main/java/mc/mec/mavenkotlintemplate/commands/HovertextCommand.kt
@@ -1,21 +1,15 @@
 package mc.mec.mavenkotlintemplate.commands
 
 import mc.mec.mavenkotlintemplate.MavenKotlinTemplate
-import net.md_5.bungee.api.chat.ClickEvent
-import net.md_5.bungee.api.chat.ComponentBuilder
-import net.md_5.bungee.api.chat.HoverEvent
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.plugin.Plugin
 
-object MavenCommand : CommandExecutor {
+object HovertextCommand: CommandExecutor {
+    val pl = MavenKotlinTemplate()
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        val pl : MavenKotlinTemplate
-        sender.sendMessage("Hello Maven!")
+        pl.sendHoverText(sender as? Player, "[ここをクリック！]","ここをクリックしてください。", "/say HoverTextはちゃんと動いてるよ。")
         return true
     }
-
 }
